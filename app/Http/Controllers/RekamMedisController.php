7a8afc5cad15ab2eeef53766eeee3eee55954195
zaki <zaki.robbany22@gmail.com>
2018-03-11 19:28:01 +0700

    Update 'rekam medis' index method : - fix daily and monthly issue

--- app/Http/Controllers/RekamMedisController.php
+++ app/Http/Controllers/RekamMedisController.php
@@ -2,51 +2,74 @@
 
 namespace App\Http\Controllers;
 
+use App\Diagnosis;
+use App\Dokter;
+use App\Obat;
 use App\PasienPoli;
 use App\RekamMedis;
+use App\RekamMedisDiagnosis;
 use App\RekamMedisObat;
+use App\Service\StoreMedicalRecord;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
-
-use App\Http\Requests;
-use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Session;
 
 class RekamMedisController extends Controller
 {
     public function index()
     {
-        $now = Carbon::now();
-        $cd = $now->day;
-        $cm = $now->month;
-        $rekams = RekamMedisObat::with('obat', 'rekamMedis')
-            ->whereDay('created_at', '=', $cd)
-            ->whereMonth('created_at', '=', $cm)
-            ->get();
+        $now = Carbon::now()->toDateString();
+        $rekams = RekamMedis::with('obat', 'diagnoses')->get();
+        $rekams = $rekams->filter(function ($rekam) use ($now) {
+            if ($rekam->created_at->toDateString() == $now) {
+                return $rekam;
+            }
+        });
+
         return view('rekammedis.index', compact('rekams'));
     }
+
     public function bulananIndex()
     {
         $now = Carbon::now();
-        $cm = $now->month;
-        $rekams = RekamMedisObat::with('obat', 'rekamMedis')->whereMonth('created_at', '=', $cm)->get();
+        $month = $now->month;
+        $year = $now->year;
+        $rekams = RekamMedisObat::with('obat', 'rekamMedis')
+            ->whereMonth('created_at', '=', $month)
+            ->whereYear('created_at', $year)
+            ->get();
+
         return view('rekammedis.bulananindex', compact('rekams'));
     }
 
     public function create($id)
     {
-        $kunjungan = PasienPoli::findOrFail($id);
-        return view('rekammedis.create', compact('kunjungan'));
+        $visit = PasienPoli::findOrFail($id);
+        $diagnoses = Diagnosis::orderBy('name', 'asc')
+            ->get();
+        $obats = Obat::orderBy('nama_obat', 'asc')
+            ->get();
+        $dokters = Dokter::where('spesialis_id', 1)
+            ->get();
+        $dentists = Dokter::where('spesialis_id', 2)
+            ->get();
+        $medicines = 1;
+        $totalDiagnoses = 1;
+
+        return view('rekammedis.create', compact('visit', 'diagnoses', 'dokters', 'dentists', 'obats'
+            , 'medicines', 'totalDiagnoses'));
 
     }
-    public function store(Request $request, $id)
+
+    public function store(Request $request)
     {
-        ##$kunjungan = PasienPoli::findOrFail($id);
+        //$kunjungan = PasienPoli::findOrFail($id);
+        dd($request->all());
         $this->validate($request, [
-            'pasien_poli_id'=>'required',
-            'diagnosa'=>'required',
-            'terapi'=>'required',
-            'obat_id'=>'required'
+            'pasien_poli_id' => 'required',
+            'diagnosa' => 'required',
+            'terapi' => 'required',
+            'obat_id' => 'required'
         ]);
         $data = $request->all();
 
@@ -56,8 +79,8 @@ class RekamMedisController extends Controller
         $rmo[] = new RekamMedisObat();
 
 
-        foreach ($request->input('obat_id') as $selected){
-            foreach ($request->input('jumlah') as $jumlah){
+        foreach ($request->input('obat_id') as $selected) {
+            foreach ($request->input('jumlah') as $jumlah) {
                 $rmo['rekam_medis_id'] = $rekam->id;
                 $rmo['obat_id'] = $selected;
                 $rmo['jumlah'] = $jumlah;
@@ -72,42 +95,35 @@ class RekamMedisController extends Controller
         return redirect()->route('dokter-rekammedis.index');
     }
 
-    public function perawatStore(Request $request, $id)
+    public function perawatStore(Request $request)
     {
-        ##$kunjungan = PasienPoli::findOrFail($id);
-        $this->validate($request, [
-            'pasien_poli_id'=>'required',
-            'diagnosa'=>'required',
-            'terapi'=>'required',
-        ]);
-        $data = $request->all();
+        $rekamMedis = new RekamMedis();
 
-        RekamMedis::create($data);
+        $data = [
+            'pasien_poli_id' => $request->pasien_poli_id,
+            'dokter_id' => $request->dokter_id,
+            'terapi' => $request->terapi,
+        ];
+        $rekamMedis->fill($data);
+        $rekamMedis->save();
 
+        $storeMedicalRecord = new StoreMedicalRecord($request);
         $rekam = RekamMedis::orderBy('created_at', 'desc')->first();
-        $rmo[] = new RekamMedisObat();
-
-
-        foreach ($request->input('array') as $selected ){
-            if ($selected['jumlah'] > 0){
-                $rmo['rekam_medis_id'] = $rekam->id;
-                $rmo['obat_id'] = $selected['obat_id'];
-                $rmo['jumlah'] = $selected['jumlah'];
-                RekamMedisObat::create($rmo);
-            }
+        if ($rekam) {
+            $storeMedicalRecord->store($rekam);
         }
-
         Session::flash("flash_notification", [
             "level" => "success",
-            "message" => "Berhasil Menambahkan Rekam Medis"
+            "message" => "Gagal Menambahkan Rekam Medis, Rekam Medis Kosong"
         ]);
+
         return redirect()->route('perawat-rekammedis.index');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -118,28 +134,37 @@ class RekamMedisController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
     {
-        $rekam = RekamMedisObat::findOrFail($id);
-        return view('rekammedis.edit', compact('rekam'));
+        $medicalRecord = RekamMedis::find($id);
+        $visit = $medicalRecord->pasienPoli;
+        $medicines = Obat::orderBy('nama_obat', 'asc')->get();
+        $diagnoses = Diagnosis::orderBy('name', 'asc')->get();
+        $dokters = Dokter::where('spesialis_id', 1)
+            ->get();
+        $dentists = Dokter::where('spesialis_id', 2)
+            ->get();
+
+        return view('rekammedis.edit', compact('medicalRecord', 'visit', 'medicines', 'diagnoses',
+            'dokters', 'dentists'));
     }
 
     public function update(Request $request, $id)
     {
         $rekam = RekamMedisObat::findOrFail($id);
         $this->validate($request, [
-            'diagnosa'=>'required',
-            'terapi'=>'required',
-            'obat'=>'required',
-            'rekam_medis_id'=>'required'
+            'diagnosa' => 'required',
+            'terapi' => 'required',
+            'obat' => 'required',
+            'rekam_medis_id' => 'required'
         ]);
         $rekam->update($request->all());
         Session::flash("flash_notification", [
-            "level"=>"success",
-            "message"=>"Berhasil Memperbarui Rekam Medis"
+            "level" => "success",
+            "message" => "Berhasil Memperbarui Rekam Medis"
         ]);
 
         return redirect()->route('dokter-rekammedis.index');
@@ -166,10 +191,9 @@ class RekamMedisController extends Controller
         $rekam->update($r->all());
 
 
-
         Session::flash("flash_notification", [
-            "level"=>"success",
-            "message"=>"Berhasil Memperbarui Rekam Medis"
+            "level" => "success",
+            "message" => "Berhasil Memperbarui Rekam Medis"
         ]);
 
         return redirect()->route('perawat-rekammedis.index');
@@ -188,7 +212,7 @@ class RekamMedisController extends Controller
 
     public function perawatDestroy($id)
     {
-        RekamMedisObat::findOrFail($id)->delete();
+        RekamMedis::findOrFail($id)->delete();
         Session::flash("flash_notification", [
             "level" => "success",
             "message" => "Rekam Medis Berhasil Dihapus"
