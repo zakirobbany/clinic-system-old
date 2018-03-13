689556d54a80bfd87e3df770e8f6d6648a7516e4
zaki <zaki.robbany22@gmail.com>
2018-03-13 19:57:13 +0700

    update home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Home;
 use App\Kpi;
 use App\Pasien;
+use App\Service\Dashboard\Visitors;
 
 class HomeController extends Controller
 {
@@ -14,79 +15,84 @@ class HomeController extends Controller
     }
     public function index()
     {
+        /*
+         * visitor instance
+         * */
+        $visitors = new Visitors();
 
-        $home = new Home();
+        /*
+         * Total Patients
+         * */
+        $totalPatients = $visitors->patients()->count();
+        $totalGeneralPatients = $visitors->generalPatients()->count();
+        $totalBPJSPatients = $visitors->KPI()->where('kpi', '=', 'pasienbpjs')->first()->bobot;
+        $totalGreenPatients = $visitors->greenPatients()->count();
+        $totalProlanisPatients = $visitors->prolanisPatients()->count();
 
-        //bulanan
+        /*
+         * Daily Visitors
+         * */
+        $todayVisitors = $visitors->todayVisitors()->count();
+        $todayGeneralVisitors = $visitors->todayGeneralVisitors()->count();
+        $todayBPJSVisitors = $visitors->todayBPJSVisitors()->count();
+        $todayGreenVisitors = $visitors->todayGreenVisitors()->count();
 
-        $totalKunjungan = $home->getJumlahKunjungan();
-        $totalPasien = $home->getJumlahPasien();
+        /*
+         * Monthly Visitors
+         * */
+        $monthVisitors = $visitors->thisMonthGeneralVisitors()->count();
+        $monthGeneralVisitors = $visitors->thisMonthGeneralVisitors()->count();
+        $monthBPJSVisitors = $visitors->thisMonthBPJSVisitors()->count();
+        $monthGreenVisitors = $visitors->thisMonthGreenVisitors()->count();
+        $monthProlanisVisitors = $visitors->thisMonthProlanisVisitors()->count();
 
-        $kunjunganUmum = $home->getKunjunganUmum();
-        $pasienUmum = $home->getPasienUmum();
+        /*
+         * KPI
+         * */
+        $generalKPI = $visitors->KPIGeneralVisitors();
+        $BPJSKPI = $visitors->KPIBPJSVisitors();
+        $prolanisKPI = $visitors->KPIProlanisVisitors();
 
-        $pasienBPJS = $home->getPasienBPJS();
-        $kunjunganBpjs = $home->getKunjunganBpjs();
-
-        $pasienKartuHijau = $home->getPasienHijau();
-        $kunjunganHijau = $home->getKunjunganHijau();
-
-        $pasienProlanis = $home->getPasienProlanis();
-        $kunjunganProlanis = $home->getKunjunganProlanis();
-
-        //Harian
-        $harian = $home->getharian();
-        $harianUmum = $home->getHarianUmum();
-        $harianBpjs = $home->getHarianBpjs();
-        $harianHijau = $home->getHarianHijau();
-
-        $totalPercentage = ($totalKunjungan / $totalPasien) * 100;
-        $umumPercentage = ($kunjunganUmum / $pasienUmum) * 100;
-        $bpjsPercentage = ($kunjunganBpjs/ $pasienBPJS) * 100;
-        $hijauPercentage = ($kunjunganHijau / $pasienKartuHijau) * 100;
-        $prolanisPercentage = ($kunjunganProlanis / $pasienProlanis) * 100;
-
-        $getBobot = Kpi::where('kpi', '=', 'kunjungan')->first();
-        $minKunjunganUmum = $getBobot->bobot;
-
-        $getBobotBpjs = Kpi::where('kpi', '=', 'bpjs')->first();
-        $minKunjunganBpjs = $getBobotBpjs->bobot;
-
-        $getBobotProlanis = Kpi::where('kpi', '=', 'prolanis')->first();
-        $minKunjunganProlanis = $getBobotProlanis->bobot;
+        /*
+         * Percentage
+         * */
+        $totalPercentage = ($monthVisitors / $totalPatients) * 100;
+        $generalPercentage = ($monthGeneralVisitors / $totalGeneralPatients) * 100;
+        $BPJSPercentage = ($monthBPJSVisitors / $totalBPJSPatients) * 100;
+        $greenPercentage = ($monthGreenVisitors / $totalGreenPatients) * 100;
+        $prolanisPercentage = ($monthProlanisVisitors / $totalProlanisPatients) * 100;
 
         $totalStatus = '';
-        if ($totalPercentage >= $minKunjunganUmum)
+        if ($totalPercentage >= $generalKPI)
             $totalStatus = 'green';
-        elseif ($totalPercentage <= $minKunjunganUmum)
+        elseif ($totalPercentage <= $generalKPI)
             $totalStatus = 'red';
 
-        $umumStatus = '';
-        if ($umumPercentage >= $minKunjunganUmum)
-            $umumStatus = 'green';
-        elseif ($umumPercentage <= $minKunjunganUmum)
-            $umumStatus = 'red';
+        $generalStatus = '';
+        if ($generalPercentage >= $generalKPI)
+            $generalStatus = 'green';
+        elseif ($generalPercentage <= $generalKPI)
+            $generalStatus = 'red';
 
-        $bpjsStatus = '';
-        if ($bpjsPercentage >= $minKunjunganBpjs)
-            $bpjsStatus = 'green';
-        elseif ($bpjsPercentage<$minKunjunganBpjs)
-            $bpjsStatus = 'red';
+        $BPJSStatus = '';
+        if ($BPJSPercentage >= $BPJSKPI)
+            $BPJSStatus = 'green';
+        elseif ($BPJSPercentage<$BPJSKPI)
+            $BPJSStatus = 'red';
 
         $prolanisStatus = '';
-        if ($prolanisPercentage >= $minKunjunganProlanis)
+        if ($prolanisPercentage >= $prolanisKPI)
             $prolanisStatus = 'green';
-        elseif ($prolanisPercentage < $minKunjunganProlanis)
+        elseif ($prolanisPercentage < $prolanisKPI)
             $prolanisStatus = 'red';
 
-
-        return view('home', compact('totalPasien', 'pasienUmum',
-            'pasienBPJS', 'pasienKartuHijau', 'pasienProlanis',
-            'totalKunjungan', 'kunjunganUmum', 'kunjunganBpjs',
-            'kunjunganHijau', 'kunjunganProlanis', 'totalPercentage',
-            'umumPercentage', 'bpjsPercentage', 'hijauPercentage', 'prolanisPercentage',
-            'totalStatus', 'umumStatus', 'bpjsStatus', 'prolanisStatus', 'kunjungans', 'polis',
-            'harian', 'harianUmum', 'harianBpjs', 'harianHijau'
+        return view('home', compact('totalPatients', 'totalGeneralPatients',
+            'totalBPJSPatients', 'totalGreenPatients', 'totalProlanisPatients',
+            'monthVisitors', 'monthGeneralVisitors', 'monthBPJSVisitors',
+            'monthGreenVisitors', 'monthProlanisVisitors', 'totalPercentage',
+            'generalPercentage', 'BPJSPercentage', 'greenPercentage', 'prolanisPercentage',
+            'totalStatus', 'generalStatus', 'BPJSStatus', 'prolanisStatus',
+            'todayVisitors', 'todayGeneralVisitors', 'todayBPJSVisitors', 'todayGreenVisitors'
         ));
     }
 
